bfe043a1961c176025db9bf3aecf7f6132cf050e
Turkova Maria <kvm1990@mail.ru>
2019-04-18 14:31:56 +0300

    flake* changes

--- db_pressure.py
+++ db_pressure.py
@@ -1,44 +1,52 @@
 import psycopg2
 from db_settings import config
 
-def create_tables():
 
-	tables = (
-		"""
-		CREATE TABLE IF NOT EXISTS users (
-		  username VARCHAR(50) PRIMARY KEY NOT NULL 
-		);
+def create_tables():
+    """
+    create 2 tables:
+    users: - username,
+    pressure: pressure_id, username, systolic, diastoic,
+    timestamp, date, arm, foreign key to users
+    """
+
+    tables = (
+        """
+        CREATE TABLE IF NOT EXISTS users (
+          username VARCHAR(50) PRIMARY KEY NOT NULL
+        );
         """,
 
-		"""
-		CREATE TABLE IF NOT EXISTS pressure(
+        """
+        CREATE TABLE IF NOT EXISTS pressure(
             pressure_id SERIAL PRIMARY KEY,
             username VARCHAR(50),
             systolic VARCHAR(3) NOT NULL,
-    		diastolic VARCHAR(3) NOT NULL,
-    		timestamp timestamptz NOT NULL,
-    		date DATE,
-    		arm VARCHAR(50) NOT NULL,
+            diastolic VARCHAR(3) NOT NULL,
+            timestamp timestamptz NOT NULL,
+            date DATE,
+            arm VARCHAR(50) NOT NULL,
             FOREIGN KEY (username) REFERENCES users
         );
-		"""
-		)
-	try:
-		connection = config()
-		cursor = connection.cursor()
+        """
+        )
+    try:
+        connection = config()
+        cursor = connection.cursor()
 
-		for table in tables:
-			cursor.execute(table)
+        for table in tables:
+            cursor.execute(table)
 
-		cursor.close()
-		connection.commit()
+        cursor.close()
+        connection.commit()
 
-	except (Exception, psycopg2.DatabaseError) as error:
-		print(error)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
 
-	finally:
-		if connection is not None:
-			connection.close()
+    finally:
+        if connection is not None:
+            connection.close()
+
 
 if __name__ == "__main__":
-	create_tables()
+    create_tables()
