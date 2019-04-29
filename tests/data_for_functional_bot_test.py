9f709082b3edc99e2794be6350fc9211c32d5f19
Turkova Maria <kvm1990@mail.ru>
2019-04-29 14:58:49 +0300

    added some tests and pep8 changes

--- tests/data_for_functional_bot_test.py
+++ tests/data_for_functional_bot_test.py
@@ -2,125 +2,127 @@ import datetime
 import psycopg2
 
 pressure_list = [
-	(108, 'Mary2z', '130', '80',
-		datetime.datetime(
-		2019, 4, 20, 12, 26, 45, 826178, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-		datetime.date(2019, 4, 20), 'l'),
+    (108, 'Mary2z', '130', '80',
+        datetime.datetime(
+        2019, 4, 20, 12, 26, 45, 826178, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+        datetime.date(2019, 4, 20), 'l'),
 
-	(109, 'Mary2z', '130', '70',
-		datetime.datetime(
-		2019, 4, 20, 13, 16, 41, 521658, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-		datetime.date(2019, 4, 20), 'r'),
+    (109, 'Mary2z', '130', '70',
+        datetime.datetime(
+        2019, 4, 20, 13, 16, 41, 521658, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+        datetime.date(2019, 4, 20), 'r'),
 
-	(117, 'Mary2z', '100', '50',
-		datetime.datetime(
-		2019, 4, 21, 22, 1, 30, 840303, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-		datetime.date(2019, 4, 21), 'l'),
+    (117, 'Mary2z', '100', '50',
+        datetime.datetime(
+        2019, 4, 21, 22, 1, 30, 840303, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+        datetime.date(2019, 4, 21), 'l'),
 
-	(118, 'Mary2z', '120', '67',
-		datetime.datetime(
-		2019, 4, 21, 22, 2, 28, 311614, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-		datetime.date(2019, 4, 21), 'l'),
+    (118, 'Mary2z', '120', '67',
+        datetime.datetime(
+        2019, 4, 21, 22, 2, 28, 311614, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+        datetime.date(2019, 4, 21), 'l'),
 
-	(191, 'Mary2z', '123', '67',
-		datetime.datetime(
-		2019, 4, 22, 15, 44, 39, 444169, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-		datetime.date(2019, 4, 22), 'r'),
+    (191, 'Mary2z', '123', '67',
+        datetime.datetime(
+        2019, 4, 22, 15, 44, 39, 444169, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+        datetime.date(2019, 4, 22), 'r'),
 
-	(192, 'Mary2z', '123', '67',
-		datetime.datetime(
-		2019, 4, 22, 15, 46, 58, 363557, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-		datetime.date(2019, 4, 22), 'l'),
+    (192, 'Mary2z', '123', '67',
+        datetime.datetime(
+        2019, 4, 22, 15, 46, 58, 363557, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+        datetime.date(2019, 4, 22), 'l'),
 
-	(194, 'Mary2z', '213', '42',
-		datetime.datetime(
-		2019, 4, 22, 15, 49, 17, 999382, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-		datetime.date(2019, 4, 22), 'r')
+    (194, 'Mary2z', '213', '42',
+        datetime.datetime(
+        2019, 4, 22, 15, 49, 17, 999382, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+        datetime.date(2019, 4, 22), 'r')
 ]
 
 
 pressure_list_new = [
-		[
-			(109, 'Mary2z', '130', '70',
-			datetime.datetime(
-			2019, 4, 20, 13, 16, 41, 521658, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)), 
-			datetime.date(2019, 4, 20), 'r')
-		],
-		[
-			(191, 'Mary2z', '123', '67',
-			datetime.datetime(
-			2019, 4, 22, 15, 44, 39, 444169, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-			datetime.date(2019, 4, 22), 'r'),
-			(194, 'Mary2z', '213', '42',
-			datetime.datetime(
-			2019, 4, 22, 15, 49, 17, 999382, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-			datetime.date(2019, 4, 22), 'r')
-		]
-	]
+        [
+            (109, 'Mary2z', '130', '70',
+                datetime.datetime(
+                2019, 4, 20, 13, 16, 41, 521658, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+                datetime.date(2019, 4, 20), 'r')
+        ],
+        [
+            (191, 'Mary2z', '123', '67',
+                datetime.datetime(
+                2019, 4, 22, 15, 44, 39, 444169, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+                datetime.date(2019, 4, 22), 'r'),
+            (194, 'Mary2z', '213', '42',
+                datetime.datetime(
+                2019, 4, 22, 15, 49, 17, 999382, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+                datetime.date(2019, 4, 22), 'r')
+        ]
+    ]
 
 empty_pressure_list = []
 
 short_pressure_list = [
-			(109, 'Mary2z', '130', '70',
-			datetime.datetime(
-			2019, 4, 20, 13, 16, 41, 521658, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)), 
-			datetime.date(2019, 4, 20), 'l')
-			]
+            (109, 'Mary2z', '130', '70',
+                datetime.datetime(
+                2019, 4, 20, 13, 16, 41, 521658, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+                datetime.date(2019, 4, 20), 'l')
+            ]
 
 systolic_list, diastolic_list, date_list = ["130", "213"], ["70", "42"], ["2019-04-20", "2019-04-22"]
 
 day_data = [
-			(191, 'Mary2z', '123', '67',
-			datetime.datetime(
-			2019, 4, 22, 15, 44, 39, 444169, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-			datetime.date(2019, 4, 22), 'r'),
-			(194, 'Mary2z', '213', '42',
-			datetime.datetime(
-			2019, 4, 22, 15, 49, 17, 999382, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-			datetime.date(2019, 4, 22), 'r')
-		]
+            (191, 'Mary2z', '123', '67',
+                datetime.datetime(
+                2019, 4, 22, 15, 44, 39, 444169, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+                datetime.date(2019, 4, 22), 'r'),
+            (194, 'Mary2z', '213', '42',
+                datetime.datetime(
+                2019, 4, 22, 15, 49, 17, 999382, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+                datetime.date(2019, 4, 22), 'r')
+        ]
 
 day_data_v2 = [
-			(191, 'Mary2z', '123', '67',
-			datetime.datetime(
-			2019, 4, 22, 15, 44, 39, 444169, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-			datetime.date(2019, 4, 22), 'r'),
-			(194, 'Mary2z', '123', '68',
-			datetime.datetime(
-			2019, 4, 22, 15, 49, 17, 999382, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-			datetime.date(2019, 4, 22), 'r')
-		]
+            (191, 'Mary2z', '123', '67',
+                datetime.datetime(
+                2019, 4, 22, 15, 44, 39, 444169, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+                datetime.date(2019, 4, 22), 'r'),
+            (194, 'Mary2z', '123', '68',
+                datetime.datetime(
+                2019, 4, 22, 15, 49, 17, 999382, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+                datetime.date(2019, 4, 22), 'r')
+        ]
 
 day_data_v3 = [
-			(191, 'Mary2z', 'b', 'a',
-			datetime.datetime(
-			2019, 4, 22, 15, 44, 39, 444169, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-			datetime.date(2019, 4, 22), 'r'),
-			(194, 'Mary2z', '123', '68',
-			datetime.datetime(
-			2019, 4, 22, 15, 49, 17, 999382, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-			datetime.date(2019, 4, 22), 'r')
-		]
+            (191, 'Mary2z', 'b', 'a',
+                datetime.datetime(
+                2019, 4, 22, 15, 44, 39, 444169, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+                datetime.date(2019, 4, 22), 'r'),
+            (194, 'Mary2z', '123', '68',
+                datetime.datetime(
+                2019, 4, 22, 15, 49, 17, 999382, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+                datetime.date(2019, 4, 22), 'r')
+        ]
 
 day_data_v4 = [
-			(191, 'Mary2z', b'Byte', 'a',
-			datetime.datetime(
-			2019, 4, 22, 15, 44, 39, 444169, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-			datetime.date(2019, 4, 22), 'r'),
-			(194, 'Mary2z', '123', '68',
-			datetime.datetime(
-			2019, 4, 22, 15, 49, 17, 999382, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-			datetime.date(2019, 4, 22), 'r')
-		]
+            (191, 'Mary2z', b'Byte', 'a',
+                datetime.datetime(
+                2019, 4, 22, 15, 44, 39, 444169, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+                datetime.date(2019, 4, 22), 'r'),
+            (194, 'Mary2z', '123', '68',
+                datetime.datetime(
+                2019, 4, 22, 15, 49, 17, 999382, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+                datetime.date(2019, 4, 22), 'r')
+        ]
 
 arm_list = [
-	["130", "150", "140", "160", "150" ],
-	["90", "90", "70", "90", "90"],
-	["15:28", "15:30", "15:41", "15:46", "15:49"],
-	'r']
+        ["130", "150", "140", "160", "150"],
+        ["90", "90", "70", "90", "90"],
+        ["15:28", "15:30", "15:41", "15:46", "15:49"],
+        'r'
+    ]
 
 arm_list_v2 = [
-	["130", "150", "140", "160", "150" ],
-	["90", "90", "70", "90", "90"],
-	["15:28", "15:30", "15:41", "15:46", "15:49"],
-	'f']
+        ["130", "150", "140", "160", "150"],
+        ["90", "90", "70", "90", "90"],
+        ["15:28", "15:30", "15:41", "15:46", "15:49"],
+        'f'
+    ]
--- tests/test_functional_bot.py
+++ tests/test_functional_bot.py
@@ -26,35 +26,36 @@ from .data_for_functional_bot_test import (
 from functional_bot import (
     make_list_for_arm,
     prepare_data_from_potgresql_to_graph,
-    save_pressure_to_postgresql,
     find_dates_in_period,
     select_data_from_postgresql,
     arm_corrector,
     create_graph,
     append_to_lists,
     find_biggest_value_per_day,
-    marking_on_coordinate_axes
+    marking_on_coordinate_axes,
+    select_data_picked_by_dates
     )
 
 from db_settings import config
+
 
 def test_make_list_for_arm():
     assert make_list_for_arm(pressure_list, "r") == [
         [
             (109, 'Mary2z', '130', '70',
-            datetime.datetime(
-            2019, 4, 20, 13, 16, 41, 521658, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)), 
-            datetime.date(2019, 4, 20), 'r')
+                datetime.datetime(
+                2019, 4, 20, 13, 16, 41, 521658, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+                datetime.date(2019, 4, 20), 'r')
         ],
         [
             (191, 'Mary2z', '123', '67',
-            datetime.datetime(
-            2019, 4, 22, 15, 44, 39, 444169, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-            datetime.date(2019, 4, 22), 'r'),
+                datetime.datetime(
+                2019, 4, 22, 15, 44, 39, 444169, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+                datetime.date(2019, 4, 22), 'r'),
             (194, 'Mary2z', '213', '42',
-            datetime.datetime(
-            2019, 4, 22, 15, 49, 17, 999382, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
-            datetime.date(2019, 4, 22), 'r')
+                datetime.datetime(
+                2019, 4, 22, 15, 49, 17, 999382, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+                datetime.date(2019, 4, 22), 'r')
         ]
     ]
 
@@ -65,9 +66,8 @@ def test_make_list_for_arm():
     assert make_list_for_arm("smth", "r") == []
 
 
-
 def test_prepare_data_from_potgresql_to_graph(monkeypatch):
-    
+
     def make_list_for_arm(pressure_list, arm):
         pressure_list_new_edited = copy.deepcopy(pressure_list_new)
         return pressure_list_new_edited
@@ -80,20 +80,23 @@ def test_prepare_data_from_potgresql_to_graph(monkeypatch):
         pressure_list_new_edited_v3 = copy.deepcopy(short_pressure_list)
         return pressure_list_new_edited_v3
 
-
     monkeypatch.setattr(functional_bot, 'make_list_for_arm', make_list_for_arm)
-    assert prepare_data_from_potgresql_to_graph(pressure_list, "r") == (["130", "213"], ["70", "42"], ["2019-04-20", "2019-04-22"], "r")
-    
+    assert prepare_data_from_potgresql_to_graph(pressure_list, "r") == (
+            ["130", "213"], ["70", "42"], ["2019-04-20", "2019-04-22"], "r"
+        )
+
     monkeypatch.setattr(functional_bot, 'make_list_for_arm', make_list_for_arm_v2)
     with pytest.raises(ValueError) as error:
         assert prepare_data_from_potgresql_to_graph(pressure_list, "r") in str(error.value)
 
     monkeypatch.setattr(functional_bot, 'make_list_for_arm', make_list_for_arm_v3)
-    assert prepare_data_from_potgresql_to_graph(pressure_list, "l") == (["130"], ["70"], ["13:16"], "l")
+    assert prepare_data_from_potgresql_to_graph(pressure_list, "l") == (
+            ["130"], ["70"], ["13:16"], "l"
+        )
 
 
 def test_find_biggest_value_per_day():
-    assert find_biggest_value_per_day(day_data)== ('213', '42')
+    assert find_biggest_value_per_day(day_data) == ('213', '42')
     assert find_biggest_value_per_day(day_data_v2) == ('123', '68')
 
     with pytest.raises(ValueError) as error:
@@ -111,7 +114,7 @@ def test_find_biggest_value_per_day():
 
 def test_append_to_lists():
     assert append_to_lists(
-        [], [], [], "130", "90", "2019-04-21"
+            [], [], [], "130", "90", "2019-04-21"
         ) == (["130"], ["90"], ["2019-04-21"])
 
     assert append_to_lists(
@@ -124,29 +127,29 @@ def test_append_to_lists():
         )
 
     assert append_to_lists(
-        [], [], [], 100, "90", "2019-04-21"
+            [], [], [], 100, "90", "2019-04-21"
         ) == ([100], ["90"], ["2019-04-21"])
 
     assert append_to_lists(
-        [], [], [], ["a", "b", "c"], "90", "2019-04-21"
+            [], [], [], ["a", "b", "c"], "90", "2019-04-21"
         ) == ([["a", "b", "c"]], ["90"], ["2019-04-21"])
 
     assert append_to_lists(
-        [], [], [], ["a", "b", "c"], {"a":1, "b":2}, "2019-04-21"
-        ) == ([["a", "b", "c"]], [{"a":1, "b":2}], ["2019-04-21"])
+            [], [], [], ["a", "b", "c"], {"a": 1, "b": 2}, "2019-04-21"
+        ) == ([["a", "b", "c"]], [{"a": 1, "b": 2}], ["2019-04-21"])
 
     assert append_to_lists(
-        [], [], [], "smth", "90", "2019-04-21"
+            [], [], [], "smth", "90", "2019-04-21"
         ) == (["smth"], ["90"], ["2019-04-21"])
 
     with pytest.raises(AttributeError) as error:
         assert append_to_lists(
-        {}, [], [], "130", "90", "2019-04-21"
+            {}, [], [], "130", "90", "2019-04-21"
         ) in str(error.value)
 
     with pytest.raises(TypeError) as error:
         assert append_to_lists(
-        [], [], [], "130", "90",
+            [], [], [], "130", "90",
         ) in str(error.value)
 
     with pytest.raises(TypeError) as error:
@@ -159,12 +162,11 @@ def test_save_pressure_to_postgresql():
         assert True
         connection.close()
 
-    #save_pressure_to_postgresql(
-    #    username, systolic, diastolic, timestamp, date, arm
-    #    )
 
 def test_find_dates_in_period():
-    assert find_dates_in_period("21.04.2019", "22.04.2019") == ["2019-04-21", "2019-04-22"]
+    assert find_dates_in_period("21.04.2019", "22.04.2019") == [
+        "2019-04-21", "2019-04-22"
+    ]
     assert find_dates_in_period("21.04.2019", "21.04.2019") == ["2019-04-21"]
     assert find_dates_in_period("01.01.1001", "01.01.1001") == ["1001-01-01"]
 
@@ -193,12 +195,66 @@ def test_select_data_from_postgresql():
     assert select_data_from_postgresql('smb') == []
 
 
-def test_find_requested_data_from_all_of_db():
-    pass
-    #find_requested_data_from_all_of_db(pressure_data, date_generated)
+def test_select_data_picked_by_dates():
+    assert select_data_picked_by_dates(pressure_list, ["2019-04-20"]) == [
+        (108, 'Mary2z', '130', '80',
+            datetime.datetime(
+            2019, 4, 20, 12, 26, 45, 826178, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+            datetime.date(2019, 4, 20), 'l'),
+
+        (109, 'Mary2z', '130', '70',
+            datetime.datetime(
+            2019, 4, 20, 13, 16, 41, 521658, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+            datetime.date(2019, 4, 20), 'r')
+        ]
+
+    assert select_data_picked_by_dates(pressure_list, ["2019-04-20", "2019-04-21"]) == [
+        (108, 'Mary2z', '130', '80',
+        datetime.datetime(
+        2019, 4, 20, 12, 26, 45, 826178, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+        datetime.date(2019, 4, 20), 'l'),
+
+        (109, 'Mary2z', '130', '70',
+            datetime.datetime(
+            2019, 4, 20, 13, 16, 41, 521658, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+            datetime.date(2019, 4, 20), 'r'),
+
+        (117, 'Mary2z', '100', '50',
+            datetime.datetime(
+            2019, 4, 21, 22, 1, 30, 840303, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+            datetime.date(2019, 4, 21), 'l'),
+
+        (118, 'Mary2z', '120', '67',
+            datetime.datetime(
+            2019, 4, 21, 22, 2, 28, 311614, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=180, name=None)),
+            datetime.date(2019, 4, 21), 'l')
+    ]
+
+    with pytest.raises(TypeError) as error:
+        assert select_data_picked_by_dates(pressure_list, []) in str(error.value)
+
+    with pytest.raises(TypeError) as error:
+        assert select_data_picked_by_dates(pressure_list, {}) in str(error.value)
+
+    with pytest.raises(TypeError) as error:
+        assert select_data_picked_by_dates("smth", {}) in str(error.value)
+
+    with pytest.raises(TypeError) as error:
+        assert select_data_picked_by_dates([1, 2, 2], {}) in str(error.value)
+
+    with pytest.raises(TypeError) as error:
+        assert select_data_picked_by_dates(
+                pressure_list,
+                [datetime.datetime(2019, 4, 20, 12, 26, 45, 826178)]
+            ) in str(error.value)
+
+    with pytest.raises(ValueError) as error:
+        assert select_data_picked_by_dates(
+            pressure_list, ["00.00.00"]) in str(error.value)
+
 
 def test_arm_corrector():
-    assert arm_corrector('Right') == 'r' 
+    assert arm_corrector('Right') == 'r'
     assert arm_corrector('Left') == 'l'
     with pytest.raises(NameError) as error:
         assert arm_corrector('smth') in str(error.value)
@@ -206,11 +262,11 @@ def test_arm_corrector():
     with pytest.raises(NameError) as error:
         assert arm_corrector(1234) in str(error.value)
 
+    with pytest.raises(NameError) as error:
+        assert arm_corrector([1, 2, 3, 'c']) in str(error.value)
+
     with pytest.raises(TypeError) as error:
         assert arm_corrector() in str(error.value)
-
-    with pytest.raises(NameError) as error:
-        assert arm_corrector([1,2,3,'c']) in str(error.value)
 
     with pytest.raises(TypeError) as error:
         assert arm_corrector('Right', "Left") in str(error.value)
@@ -219,15 +275,16 @@ def test_arm_corrector():
 def test_create_graph():
     create_graph(arm_list)
 
-    script_path = path.abspath(path.join(os.getcwd(),"../"))
+    script_path = path.abspath(path.join(os.getcwd(), "../"))
     graph_path = "Right arm_graph.png"
     test_graph_path = "test_graph.png"
 
     abs_graph_path = os.path.join(script_path, graph_path)
-    abs_test_graph_path = os.path.join(script_path, test_graph_path)    
+    abs_test_graph_path = os.path.join(script_path, test_graph_path)
 
-    with open(abs_graph_path,'rb') as f1, \
-        open(abs_test_graph_path,'rb') as f2:
+    with open(abs_graph_path, 'rb') as f1, \
+        open(abs_test_graph_path, 'rb') as f2:
+
         while True:
             b1, b2 = f1.read(1), f2.read(1)
             if not b1 or not b2:
@@ -248,9 +305,9 @@ def test_create_graph():
 
 def test_marking_on_coordinate_axes():
     assert marking_on_coordinate_axes(arm_list) == (
-        "Right arm",
-        [130, 150, 140, 160, 150 ],
-        [90, 90, 70, 90, 90]
+            "Right arm",
+            [130, 150, 140, 160, 150],
+            [90, 90, 70, 90, 90]
         )
 
     with pytest.raises(ValueError) as error:
@@ -264,4 +321,3 @@ def test_marking_on_coordinate_axes():
 
     with pytest.raises(TypeError) as error:
         assert marking_on_coordinate_axes('a', 'b', 'c') in str(error.value)
-
