07488f3e87068067cbe5c363749ba615af3dad24
Turkova Maria <kvm1990@mail.ru>
2019-05-23 15:39:19 +0300

    states of handlers in the class

---
+++ states.py
@@ -0,0 +1,12 @@
+class States:
+    START = 1
+    AGE = 2
+    SEX = 3
+    WEIGHT = 4
+    ADD_PRESSURE = 5
+    ARM = 6
+    PRESSURE = 7
+    GRAPH_FOR_PERIOD = 8
+    SET_TIMER = 9
+    REMOVE_TIMER = 10
+    START_BUTTON = 11
